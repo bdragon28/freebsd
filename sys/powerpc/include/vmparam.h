719f9963eca7b075e82076c1265409efebe9206b
mmacy <mmacy@FreeBSD.org>
2018-10-09 17:42:20 -0700

    implement pmap_kenter, handle early growkernel

--- sys/powerpc/include/vmparam.h
+++ sys/powerpc/include/vmparam.h
@@ -86,20 +86,19 @@
  * page table indexes from pmap.h for precision.
  *
  * 0x0000000000000000 - 0x00007fffffffffff   user map
- * 0xc000000000000000 - 0xc0000fffffffffff   direct map
- * 0xc000100000000000 - 0xc0001fffffffffff   kernel map
- * 0xc001000000000000 - 0xc001000dffffffff   recursive page table (8TB slot)
+ * 0xc000000000000000 - 0xc003ffffffffffff   direct map
+ * 0xc004000000000000 - 0xc00fffffffffffff   kernel map
  *
  */
 
-#define	KPML0BASE		0xc001000000000000
-#define	PML0SHIFT		39
+#define	KPML0BASE			0xc001000000000000
+#define	PML0SHIFT			39
 
-#define	VM_MIN_ADDRESS		0x0000000000000000
-#define	VM_MAXUSER_ADDRESS	0x3ffffffffffff000
-#define	VM_MAX_ADDRESS		0xffffffffffffffff
-#define	VM_MIN_KERNEL_ADDRESS		0xe000000000000000
-#define	VM_MAX_KERNEL_ADDRESS		0xe0000007ffffffff
+#define	VM_MIN_ADDRESS			0x0000000000000000
+#define	VM_MAXUSER_ADDRESS		0x3ffffffffffff000
+#define	VM_MAX_ADDRESS			0xffffffffffffffff
+#define	VM_MIN_KERNEL_ADDRESS		0xc004000000000000
+#define	VM_MAX_KERNEL_ADDRESS		0xc00fffffffffffff
 #define	VM_MAX_SAFE_KERNEL_ADDRESS	VM_MAX_KERNEL_ADDRESS
 #else
 #define	VM_MIN_ADDRESS		0
