f96ad00e93abed2acc647c8f5d4c4aa6378f174f
Matt Macy <mmacy@mattmacy.io>
2018-10-20 15:51:47 -0700

    add context to traps, filter read and gettimeofday from syscall printfs

--- sys/powerpc/powerpc/trap.c
+++ sys/powerpc/powerpc/trap.c
@@ -262,6 +262,7 @@ trap(struct trapframe *frame)
 #if defined(__powerpc64__) && defined(AIM)
 		case EXC_ISE:
 		case EXC_DSE:
+			trap_fatal(frame);
 			if (handle_user_slb_spill(&p->p_vmspace->vm_pmap,
 			    (type == EXC_ISE) ? frame->srr0 : frame->dar) != 0){
 				sig = SIGSEGV;
@@ -557,8 +558,9 @@ printtrap(u_int vector, struct trapframe *frame, int isfatal, int user)
 	    frame->lr, frame->lr - (register_t)(__startkernel - KERNBASE));
 	printf("   curthread       = %p\n", curthread);
 	if (curthread != NULL)
-		printf("          pid = %d, comm = %s\n",
-		    curthread->td_proc->p_pid, curthread->td_name);
+		printf("          pid = %d, comm = %s asid: %lu\n",
+			   curthread->td_proc->p_pid, curthread->td_name,
+			   vmspace_pmap(curthread->td_proc->p_vmspace)->pm_pid);
 	printf("\n");
 }
 
@@ -841,6 +843,7 @@ trap_pfault(struct trapframe *frame, int user)
 		return (0);
 
 	printf("%s vm_fault=>rv = %d\n", __func__, rv);
+	printtrap(frame->exc, frame, 0, (frame->srr1 & PSL_PR));
 	if (!user && handle_onfault(frame))
 		return (0);
 
