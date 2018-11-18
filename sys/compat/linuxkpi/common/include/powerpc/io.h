c4fd16b2b171bc5b15be298dfab4a7cd285ab68c
Matt Macy <mmacy@FreeBSD.org>
2018-11-18 03:45:26 -0800

    reinstate _relaxed functions

--- sys/compat/linuxkpi/common/include/powerpc/io.h
+++ sys/compat/linuxkpi/common/include/powerpc/io.h
@@ -7,4 +7,14 @@
 #else
 #define mmiowb()
 #endif
+
+#define readb_relaxed(addr)	readb(addr)
+#define readw_relaxed(addr)	readw(addr)
+#define readl_relaxed(addr)	readl(addr)
+#define readq_relaxed(addr)	readq(addr)
+#define writeb_relaxed(v, addr)	writeb(v, addr)
+#define writew_relaxed(v, addr)	writew(v, addr)
+#define writel_relaxed(v, addr)	writel(v, addr)
+#define writeq_relaxed(v, addr)	writeq(v, addr)
+
 #endif
