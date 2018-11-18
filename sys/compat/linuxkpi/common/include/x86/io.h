269154d6cfbc7f5e46c0ddb71736669f88515a95
Matt Macy <mmacy@FreeBSD.org>
2018-11-18 03:45:26 -0800

    reinstate _relaxed functions

--- sys/compat/linuxkpi/common/include/x86/io.h
+++ sys/compat/linuxkpi/common/include/x86/io.h
@@ -7,4 +7,14 @@ _outb(u_char data, u_int port)
 {
 	__asm __volatile("outb %0, %w1" : : "a" (data), "Nd" (port));
 }
+
+#define readb_relaxed(a) __raw_readb(a)
+#define readw_relaxed(a) __raw_readw(a)
+#define readl_relaxed(a) __raw_readl(a)
+#define readq_relaxed(a) __raw_readq(a)
+
+#define writeb_relaxed(v, a) __raw_writeb(v, a)
+#define writew_relaxed(v, a) __raw_writew(v, a)
+#define writel_relaxed(v, a) __raw_writel(v, a)
+#define writeq_relaxed(v, a) __raw_writeq(v, a)
 #endif
