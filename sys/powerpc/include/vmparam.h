mattmacy/networking

revert KERNBASE

sys/powerpc/include/vmparam.h
@@ -127,7 +127,7 @@
 #endif
 
 #ifdef AIM
-#define	KERNBASE		0x20000000UL	/* start of kernel virtual */
+#define	KERNBASE		0x00100100UL    /* start of kernel virtual */
 
 #ifndef __powerpc64__
 #define	VM_MIN_KERNEL_ADDRESS	((vm_offset_t)KERNEL_SR << ADDR_SR_SHFT)

bdragon28/freebsd

sys/powerpc/include/vmparam.h
@@ -131,7 +131,7 @@
 #endif
 
 #ifdef AIM
-#define	KERNBASE		0x20000000	/* start of kernel virtual */
+#define	KERNBASE		0x00100100    /* start of kernel virtual */
 
 #ifndef __powerpc64__
 #define	VM_MIN_KERNEL_ADDRESS	((vm_offset_t)KERNEL_SR << ADDR_SR_SHFT)