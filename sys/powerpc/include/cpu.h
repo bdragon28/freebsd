mattmacy/networking

add radix mmu stubs

sys/powerpc/include/cpu.h
@@ -89,6 +89,7 @@ extern u_long cpu_features2;
 #define	PPC_FEATURE2_HAS_IEEE128	0x00400000
 #define	PPC_FEATURE2_DARN	0x00200000
 #define	PPC_FEATURE2_SCV	0x00100000
+#define	PPC_FEATURE2_MMU_RADIX	0x00080000
 #define	PPC_FEATURE2_HTM_NOSUSPEND	0x01000000
 
 #define	PPC_FEATURE_BITMASK						\

bdragon28/freebsd

sys/powerpc/include/cpu.h
@@ -91,6 +91,7 @@ extern u_long cpu_features2;
 #define	PPC_FEATURE2_DARN	0x00200000
 #define	PPC_FEATURE2_SCV	0x00100000
 #define	PPC_FEATURE2_HTM_NOSUSPEND	0x00080000
+#define	PPC_FEATURE2_MMU_RADIX	0x00100000
 
 #define	PPC_FEATURE_BITMASK						\
 	"\20"								\