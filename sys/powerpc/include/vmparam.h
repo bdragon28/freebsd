mattmacy/networking

implement support for vm_mem_init up through pmap_init

sys/powerpc/include/vmparam.h
@@ -111,15 +111,18 @@
  * Virtual addresses of things.  Derived from the page directory and
  * page table indexes from pmap.h for precision.
  *
+ * kernel map should be able to start at 0xc008000000000000 -
+ * but at least the functional simulator doesn't like it
+ *
  * 0x0000000000000000 - 0x00007fffffffffff   user map
  * 0xc000000000000000 - 0xc003ffffffffffff   direct map 
- * 0xc004000000000000 - 0xc00fffffffffffff   kernel map
+ * 0xc004000000000000 - 0xc007ffffffffffff   kernel map
  *
  */
 
 
 #define	VM_MIN_KERNEL_ADDRESS		0xc004000000000000UL
-#define	VM_MAX_KERNEL_ADDRESS		0xc00fffffffffffffUL
+#define	VM_MAX_KERNEL_ADDRESS		0xc007ffffffffffffUL
 #define	VM_MAX_SAFE_KERNEL_ADDRESS	VM_MAX_KERNEL_ADDRESS
 #endif
 
@@ -163,7 +166,7 @@ struct pmap_physseg {
 };
 #endif
 
-#define	VM_PHYSSEG_MAX		16	/* 1? */
+#define	VM_PHYSSEG_MAX		63	/* 1? */
 
 /*
  * The physical address space is densely populated on 32-bit systems,
@@ -257,6 +260,7 @@ struct pmap_physseg {
 #ifndef LOCORE
 #ifdef __powerpc64__
 #define	DMAP_BASE_ADDRESS	0xc000000000000000UL
+#define	DMAP_MIN_ADDRESS	DMAP_BASE_ADDRESS
 #define	DMAP_MAX_ADDRESS	0xcfffffffffffffffUL
 #else
 #define	DMAP_BASE_ADDRESS	0x00000000UL

bdragon28/freebsd

sys/powerpc/include/vmparam.h
@@ -85,9 +85,12 @@
  * Virtual addresses of things.  Derived from the page directory and
  * page table indexes from pmap.h for precision.
  *
+ * kernel map should be able to start at 0xc008000000000000 -
+ * but at least the functional simulator doesn't like it
+ *
  * 0x0000000000000000 - 0x00007fffffffffff   user map
  * 0xc000000000000000 - 0xc003ffffffffffff   direct map
- * 0xc004000000000000 - 0xc00fffffffffffff   kernel map
+ * 0xc004000000000000 - 0xc007ffffffffffff   kernel map
  *
  */
 
@@ -98,7 +101,7 @@
 #define	VM_MAXUSER_ADDRESS		0x3ffffffffffff000
 #define	VM_MAX_ADDRESS			0xffffffffffffffff
 #define	VM_MIN_KERNEL_ADDRESS		0xc004000000000000
-#define	VM_MAX_KERNEL_ADDRESS		0xc00fffffffffffff
+#define	VM_MAX_KERNEL_ADDRESS		0xc007ffffffffffff
 #define	VM_MAX_SAFE_KERNEL_ADDRESS	VM_MAX_KERNEL_ADDRESS
 #else
 #define	VM_MIN_ADDRESS		0
@@ -146,7 +149,7 @@ struct pmap_physseg {
 };
 #endif
 
-#define	VM_PHYSSEG_MAX		16
+#define	VM_PHYSSEG_MAX		63	/* 1? */
 
 #define	PHYS_AVAIL_SZ	256	/* Allows up to 16GB Ram on pSeries with
 				 * logical memory block size of 64MB.
@@ -248,6 +251,7 @@ struct pmap_physseg {
 #ifndef LOCORE
 #ifdef __powerpc64__
 #define	DMAP_BASE_ADDRESS	0xc000000000000000UL
+#define	DMAP_MIN_ADDRESS	DMAP_BASE_ADDRESS
 #define	DMAP_MAX_ADDRESS	0xcfffffffffffffffUL
 #else
 #define	DMAP_BASE_ADDRESS	0x00000000UL