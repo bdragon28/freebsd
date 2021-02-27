02613f3876c0e42e5b56c6b07f0bd7f5be51327d
Brandon Bergren <bdragon@FreeBSD.org>
2021-02-26 18:46:46 -0600

    Backport D93919: [PowerPC] Support powerpcle target in Clang [3/5]

--- contrib/llvm-project/clang/lib/Basic/Targets/OSTargets.h
+++ contrib/llvm-project/clang/lib/Basic/Targets/OSTargets.h
@@ -252,6 +252,7 @@ public:
     case llvm::Triple::mips:
     case llvm::Triple::mipsel:
     case llvm::Triple::ppc:
+    case llvm::Triple::ppcle:
     case llvm::Triple::ppc64:
     case llvm::Triple::ppc64le:
       this->MCountName = "_mcount";
@@ -408,6 +409,7 @@ public:
     case llvm::Triple::mips64:
     case llvm::Triple::mips64el:
     case llvm::Triple::ppc:
+    case llvm::Triple::ppcle:
     case llvm::Triple::ppc64:
     case llvm::Triple::ppc64le:
       this->MCountName = "_mcount";
